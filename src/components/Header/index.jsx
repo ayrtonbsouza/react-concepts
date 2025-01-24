5eb93fbbcd73867f193f4393f50f396b289053e4
Ayrton Souza <ayrtonsouza@icloud.com>
2025-01-24 17:22:12 -0300

    [:sparkles:] header component development

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,9 +1,11 @@
 import styles from './Header.module.css';
 
+import logo from '../../assets/logo.svg';
+
 export function Header() {
   return (
     <header className={styles.header}>
-      <strong className={styles.header}>Feed</strong>
+      <img src={logo} alt="Logo" />
     </header>
   )
 }
