386e46fa4fca89edd5910323791112f598c228af
Ayrton Souza <ayrtonsouza@icloud.com>
2025-01-24 13:18:34 -0300

    [:sparkles:] adding property parse on component

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,10 @@ export function App() {
   return (
     <div>
       <h1>Initial Project</h1>
-      <Post />
+      <Post 
+        author="Ayrton Souza" 
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium accusamus cumque sit, fugiat tempora voluptas placeat culpa quos. Corporis porro neque corrupti quis dolores aut unde deleniti a esse consectetur." 
+      />
     </div>
   )
 }
-
