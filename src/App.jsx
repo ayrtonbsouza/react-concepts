2c4a4726c8deae7019faadab37c7154c0abfd847
Ayrton Souza <ayrtonsouza@icloud.com>
2025-01-24 18:09:54 -0300

    [:construction:] initial sidebar development

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
-import { Header } from "./components"
-
+import { Header, Sidebar } from "./components"
+import styles from './App.module.css'
 import './styles/global.css'
 
 export function App() {
   return (
     <div>
       <Header />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>Main</main>
+      </div>
     </div>
   )
 }
