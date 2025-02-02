5459be78f9ae1b75c4b2aec2d62d0eab0bff7907
Ayrton Souza <ayrtonsouza@icloud.com>
2025-02-01 23:19:51 -0300

    [:construction:] sending post data to component using properties

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,37 @@ import styles from './App.module.css'
 import './styles/global.css'
 import {} from "./components/Post"
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/ayrtonbsouza.png',
+      name: 'Ayrton Souza',
+      role: 'Software Engineer'
+    },
+    content : [
+      { type: 'paragraph', content: 'Sup, guys! 👋🏼' },
+      { type: 'paragraph', content: "I just uploaded a new project to my porfolio. It's a project that I developed on Ignite. One of the Rocketseat trails. The project name is Feed" },
+      { type: 'link', content: 'jane.design/doctorcare' }
+    ],
+    publishedAt: new Date('2025-01-26 16:59:30')
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/maykbrito.png',
+      name: 'Mayk Brito',
+      role: 'Software Engineer'
+    },
+    content : [
+      { type: 'paragraph', content: 'Sup, guys! 👋🏼' },
+      { type: 'paragraph', content: "I just uploaded a new project to my porfolio. It's a project that I developed on Ignite. One of the Rocketseat trails. The project name is Feed" },
+      { type: 'link', content: 'jane.design/doctorcare' }
+    ],
+    publishedAt: new Date('2025-01-26 16:59:30')
+  }
+]
+
 export function App() {
   return (
     <div>
@@ -10,7 +41,16 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post />
+          { posts.map(post => {
+            return (
+              <Post
+                key={post.id}
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            )
+          }) }
         </main>
       </div>
     </div>
